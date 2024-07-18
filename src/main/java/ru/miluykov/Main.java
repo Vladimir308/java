d09540d513b02ff7fc4e6cd71c707f07d44bc2c7
Vladimir Milyukov <vova.milyukov.94@bk.ru>
2024-07-18 10:20:36 +0300

    add import static

--- src/main/java/ru/miluykov/Main.java
+++ src/main/java/ru/miluykov/Main.java
@@ -1,12 +1,14 @@
 package ru.miluykov;
 
+import static java.lang.System.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        out.print("Hello and welcome!");
         int a = 100;
         int b = 300;
         int c = 250;
@@ -14,33 +16,30 @@ public class Main {
         int var1 = 10;
         int var2 = 20;
         int var3 = 10;
-        System.out.println("Arithmetic operations");
-        System.out.println("a + b = " + (a + b));
-        System.out.println("a - b = " + (a - b));
-        System.out.println("a * b = " + (a * b));
-        System.out.println("b / a = " + (b / a));
-        System.out.println("b % a = " + (b % a));
-        System.out.println("c % a = " + (c % a));
-        System.out.println("a++   = " +  (a++));
-        System.out.println("a--   = " +  (a--));
-        System.out.println("d++   = " +  (d++));
-        System.out.println("++d   = " +  (++d));
-        System.out.println("////////////////////");
-        System.out.println("Logical operations");
-        if ((var1 == var2) && (var2 == var3)) {
-            System.out.println("var1, var2, var3 are equal");
-        }
-        System.out.println("var1, var2, var3 are not equal");
-        System.out.println("////////////////////");
-        System.out.println("Overflow");
+        out.println("Arithmetic operations");
+        out.println("a + b = " + (a + b));
+        out.println("a - b = " + (a - b));
+        out.println("a * b = " + (a * b));
+        out.println("b / a = " + (b / a));
+        out.println("b % a = " + (b % a));
+        out.println("c % a = " + (c % a));
+        out.println("a++   = " +  (a++));
+        out.println("a--   = " +  (a--));
+        out.println("d++   = " +  (d++));
+        out.println("++d   = " +  (++d));
+        out.println("////////////////////");
+        out.println("Logical operations");
+        out.println("var1, var2, var3 are not equal");
+        out.println("////////////////////");
+        out.println("Overflow");
         int intMax = 2147483647;
         long longMax = 9223372036854775807L;
-        System.out.println("Overflow for int type: " + (intMax+5));
-        System.out.println("Overflow for long type: " + (longMax+5));
-        System.out.println("////////////////////");
-        System.out.println("Combination int and double:");
-        System.out.println(10.0+2);
-        System.out.println("Combination string, int and double:");
-        System.out.println("Hello, world!" + 10.5+1);
+        out.println("Overflow for int type: " + (intMax+5));
+        out.println("Overflow for long type: " + (longMax+5));
+        out.println("////////////////////");
+        out.println("Combination int and double:");
+        out.println(10.0+2);
+        out.println("Combination string, int and double:");
+        out.println("Hello, world!" + 10.5+1);
         }
     }
